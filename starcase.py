136468cf87a9b3afdba5f89fa0b31eaa1980cc3a
cvrlitos quin <c.quintanillamanzano@gmail.com>
2019-05-10 13:35:06 -0400

    staircaseh

---
+++ starcase.py
@@ -0,0 +1,20 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+    str = ' '
+
+    for i in range(1,n+1):
+        print((n-i)*' ' + i*'#')
+        
+    
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
